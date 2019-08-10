dd10881b46a27750d109785d5709ce1467db92e7
qiaoshangwuxin-5932 <1160301028@qq.com>
2019-08-10 15:45:21 +0800

    防止空值

--- NcuGamer/Gamer/userAndQuestion/pushScore.py
+++ NcuGamer/Gamer/userAndQuestion/pushScore.py
@@ -13,65 +13,72 @@ def pushScore(request):
         question = req['question']
         question_id = req['question_id']
         answer = req['answer']
-        groups = req['groups']
-        question_id = int(question_id)
-        list = ['one','two','three','four','five']
-        b = list[question_id-1]
-        lastAnswer = User.objects.filter(username=username).values('%s'%(b))
-        score = User.objects.filter(username=username).values('score')
-        complete = Questions.objects.filter(groups=groups, question=question).values('scorelevel__%s'%(answer))
-        for a in complete:
-            a = a['scorelevel__%s'%(answer)]
-            for score in score:
-                score = score['score']
-                for last in lastAnswer:
-                    lastAnswer = last['%s' % (b)]
-                    if complete:
-                        a = str(a)
-                        if lastAnswer is None:
-                            User.objects.filter(username=username).update(score=F('score')+a)
-                            mad(b, username, answer, groups)
-                            if b == 'three':
-                                score = User.objects.filter(username=username, score__lte=50)
-                                if score:
-                                    return JsonResponse(
-                                        {
-                                            "status": 1,
-                                            "success": True,
-                                            "spark": True
-                                        }
-                                    )
+        if answer is None:
+            return JsonResponse(
+                {
+                    "status":0,
+                }
+            )
+        else:
+            groups = req['groups']
+            question_id = int(question_id)
+            list = ['one','two','three','four','five']
+            b = list[question_id-1]
+            lastAnswer = User.objects.filter(username=username).values('%s'%(b))
+            score = User.objects.filter(username=username).values('score')
+            complete = Questions.objects.filter(groups=groups, question=question).values('scorelevel__%s'%(answer))
+            for a in complete:
+                a = a['scorelevel__%s'%(answer)]
+                for score in score:
+                    score = score['score']
+                    for last in lastAnswer:
+                        lastAnswer = last['%s' % (b)]
+                        if complete:
+                            a = str(a)
+                            if lastAnswer is None:
+                                User.objects.filter(username=username).update(score=F('score')+a)
+                                mad(b, username, answer, groups)
+                                if b == 'three':
+                                    score = User.objects.filter(username=username, score__lte=50)
+                                    if score:
+                                        return JsonResponse(
+                                            {
+                                                "status": 1,
+                                                "success": True,
+                                                "spark": True
+                                            }
+                                        )
+                                    else:
+                                        return JsonResponse(
+                                            {
+                                                "status": 1,
+                                                "success": True,
+                                                "spark": False
+                                            }
+                                        )
                                 else:
                                     return JsonResponse(
                                         {
-                                            "status": 1,
-                                            "success": True,
-                                            "spark": False
+                                            "status":1,
+                                            "success":True,
+                                            "spark":False
                                         }
                                     )
                             else:
-                                return JsonResponse(
-                                    {
-                                        "status":1,
-                                        "success":True,
-                                        "spark":False
-                                    }
-                                )
-                        else:
-                            seqs = Questions.objects.filter(groups=groups, question=question).values('scorelevel__%s' % (lastAnswer))
-                            for seq in seqs:
-                                seq = seq['scorelevel__%s'%(lastAnswer)]
-                                seq = str(seq)
-                                User.objects.filter(username=username).update(score=F('score') + a)
-                                User.objects.filter(username=username).update(score=F('score') - seq)
-                                mad(b, username, answer, groups)
-                                return JsonResponse(
-                                    {
-                                        "status": 1,
-                                        "success": True,
-                                        "message":"修改成功",
-                                        "spark":False
-                                    }
-                                )
+                                seqs = Questions.objects.filter(groups=groups, question=question).values('scorelevel__%s' % (lastAnswer))
+                                for seq in seqs:
+                                    seq = seq['scorelevel__%s'%(lastAnswer)]
+                                    seq = str(seq)
+                                    User.objects.filter(username=username).update(score=F('score') + a)
+                                    User.objects.filter(username=username).update(score=F('score') - seq)
+                                    mad(b, username, answer, groups)
+                                    return JsonResponse(
+                                        {
+                                            "status": 1,
+                                            "success": True,
+                                            "message":"修改成功",
+                                            "spark":False
+                                        }
+                                    )
 
 
--- NcuGamer/Gamer/views.py
+++ NcuGamer/Gamer/views.py
@@ -111,25 +111,24 @@ def nine(request):
         nineAnswer = [0,1,2,3,4,5,6,7,8]
         # list = request.POST.getlist('list[]') # 数组名字
         list = simplejson.loads(request.body.decode('utf-8'))
-        for a in list:
-            list = a['puzzle']
-            print(list)
-            if list == nineAnswer:
-                return JsonResponse(
-                    {
-                        'status':1,
-                    }
-                )
-                # image = os.path.join(d, "photo/myheart.jpg")
-                # data = open(image, 'rb').read()  # 读取图片
-                # return HttpResponse(data, content_type='image/png')
-            else:
-                return JsonResponse(
-                    {
-                        'status':0,
-                        'success':False,
-                        'message':'你的拼图有错误哦'
-                    },
-                    json_dumps_params={'ensure_ascii': False}
-                )
+        list = list['puzzle']
+        if list == nineAnswer:
+            return JsonResponse(
+                {
+                    'status':1,
+                },
+                json_dumps_params={'ensure_ascii': False}
+            )
+            # image = os.path.join(d, "photo/myheart.jpg")
+            # data = open(image, 'rb').read()  # 读取图片
+            # return HttpResponse(data, content_type='image/png')
+        else:
+            return JsonResponse(
+                {
+                    'status':0,
+                    'success':False,
+                    'message':'你的拼图有错误哦'
+                },
+                json_dumps_params={'ensure_ascii': False}
+            )
 
